04a11e77eb0333a0c5f3c2c787a14f8709cdb7ce
MrLwitwma <147829440+MrLwitwma@users.noreply.github.com>
2025-03-21 00:29:34 +0530

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -17,4 +17,43 @@ document.addEventListener('DOMContentLoaded', ()=>{
             window.open(`https://dictionary.bitmindai.in?word=${word}`, "_blank");
         } 
     }
+
+
+    const navButton = document.getElementById('icon');
+    const navBar = document.getElementById('nav');
+    let isClickable = true;
+
+    navButton.addEventListener('click', () => {
+        if (!isClickable) return;
+        isClickable = false; // Disable further clicks temporarily
+
+
+        if (navBar.style.display == 'none'){
+            navButton.setAttribute('src', 'static/pane-close.svg')
+            navBar.style.display = 'block'
+        } else{
+            navButton.setAttribute('src', 'static/pane-open.svg')
+            navBar.style.display = 'none'
+        }
+
+
+        // Re-enable clicks after 0.5 seconds
+        setTimeout(() => {
+            isClickable = true;
+        }, 500);
+    });
+
+
+    function handleResize() {
+        if (window.innerWidth <= 1000) {
+            navButton.setAttribute('src', 'static/pane-open.svg')
+        }
+    }
+    
+    // Run on page load
+    handleResize();
+    // Run on window resize
+    window.addEventListener('resize', handleResize);
+    
+
 })
